c802ed430e0a7af4f0533475d4895bdd0edc53df
Sören Reimler <soeren.reimler@gmail.com>
2025-11-16 16:41:56 +0100

    Add support for container types to the container data class

--- apps/composeApp/src/commonMain/kotlin/com/sreimler/plateless/domain/Container.kt
+++ apps/composeApp/src/commonMain/kotlin/com/sreimler/plateless/domain/Container.kt
@@ -8,14 +8,27 @@ import kotlin.uuid.Uuid
  *
  * @property id The unique identifier for the container.
  * @property name The name of the container.
+ * @property type The [ContainerType] of the container, defaulting to [ContainerType.PLATE].
  * @property tareWeight The tare weight (empty weight) of the container.
+ * @property isPrimary Boolean indicating if this is the primary container used in the app.
  * @property imageUri The uri pointing to an image of the container.
  */
 @OptIn(ExperimentalUuidApi::class)
 data class Container(
     val id: Uuid = Uuid.random(),
     val name: String = "",
+    val type: ContainerType = ContainerType.PLATE,
     val tareWeight: Double = 0.0,
     val isPrimary: Boolean = false,
     val imageUri: String? = null
 )
+
+enum class ContainerType {
+    PLATE,
+    DEEP_PLATE,
+    SMALL_BOWL,
+    MIXING_BOWL,
+    MUG,
+    JAR,
+    FOOD_STORAGE_CONTAINER
+}
